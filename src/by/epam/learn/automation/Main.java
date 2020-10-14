e0abd7142ff6a0acb9ae590e57936485b13fb3f9
Siarhei Marzan <siarhei.marzan@gmail.com>
2020-10-14 16:26:59 +0300

    add task1

--- src/by/epam/learn/automation/Main.java
+++ src/by/epam/learn/automation/Main.java
@@ -3,6 +3,9 @@ package by.epam.learn.automation;
 public class Main {
 
     public static void main(String[] args) {
-
+        //1. Приветствовать любого пользователя при вводе его имени через командную строку.
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello " + name);
     }
 }
